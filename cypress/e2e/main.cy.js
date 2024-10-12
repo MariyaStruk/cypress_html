b31a4051c64693aae41bb6d8f763f6b9406beb79
Maria Struk <mariya.struck@gmail.com>
2024-10-12 17:57:53 +0300

    initiale commit

---
+++ cypress/e2e/main.cy.js
@@ -0,0 +1,22 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/src/index.html')
+    cy.contains('test123').should('be.visible')
+
+    const headerNames = ['Test2', 'Test3']
+  
+    headerNames.forEach((headerName) => {
+      checkForm(headerName)
+    })
+  })
+
+  const checkForm = (titleName) => {
+    cy.contains('h2', titleName).parent().within(() => {
+      cy.get('#text-field').type('gfgcf454')
+      cy.get('.password-field').type('123456k')
+      cy.get("[data-cy='number-field']").type('123456k')
+      cy.get("[type='checkbox']").click().click()
+    })
+  }
+
+})
